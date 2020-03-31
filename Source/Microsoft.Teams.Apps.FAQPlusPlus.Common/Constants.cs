aae6321e051ed6a4ca2f822f7a7b20d0e155da11
AmitKhot25 <33828336+AmitKhot25@users.noreply.github.com>
2020-03-31 15:59:37 +0530

    Update Constants.cs

--- Source/Microsoft.Teams.Apps.FAQPlusPlus.Common/Constants.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus.Common/Constants.cs
@@ -63,22 +63,22 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Common
         /// <summary>
         /// TeamTour - text that triggers team tour action.
         /// </summary>
-        public const string TeamTour = "team tour";
+        public const string TeamTour = "lagtur";
 
         /// <summary>
         /// TakeAtour - text that triggers take a tour action for the user.
         /// </summary>
-        public const string TakeATour = "take a tour";
+        public const string TakeATour = "ta en rundtur";
 
         /// <summary>
         /// AskAnExpert - text that renders the ask an expert card.
         /// </summary>
-        public const string AskAnExpert = "ask an expert";
+        public const string AskAnExpert = "fråga en expert";
 
         /// <summary>
         /// Feedback - text that renders share feedback card.
         /// </summary>
-        public const string ShareFeedback = "share feedback";
+        public const string ShareFeedback = "dela feedback";
 
         /// <summary>
         /// Table name where SME activity details from bot will be saved.
